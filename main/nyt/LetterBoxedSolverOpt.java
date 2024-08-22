0596f2505dcf4283b731fde47b1e5549570af976
Willowsap <willowsapphire@protonmail.com>
2024-08-21 20:00:23 -0400

    nasty

--- main/nyt/LetterBoxedSolverOpt.java
+++ main/nyt/LetterBoxedSolverOpt.java
@@ -4,88 +4,81 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class LetterBoxedSolverOpt {
-    private String[] dictionary;
-    private ArrayList<String> answers;
-    private boolean[][] used;
-    private char[][] letters;
-    public LetterBoxedSolverOpt(String dictPath) throws FileNotFoundException {
-        this.answers = new ArrayList<>();
-        Scanner dictFile = new Scanner(new File(dictPath));
-        this.dictionary = new String[Integer.parseInt(dictFile.nextLine())];
-        for (int i = 0; i < this.dictionary.length; i++)
-            this.dictionary[i] = dictFile.nextLine().toLowerCase();
-        dictFile.close();
-    }
-    public String[] findBestSolution(char[][] letters) {
-        this.letters = letters;
-        this.used = new boolean[letters.length][];
-        for (int i = 0; i < used.length; i++)
-            this.used[i] = new boolean[letters[i].length];
-        ArrayList<String> firstWords = new ArrayList<>();
-        for (int i = 0; i < letters.length; i++)
-            for (int j = 0; j < letters[i].length; j++)
-                getWords(letters[i][j] + "", firstWords);
-        for (String word : firstWords)
-            if (validSolution(word))
-                answers.add(word);
-        if (answers.size() == 0) {
-            answers = firstWords;
-            int best = 2;
-            do solveLength(best++);
-            while (answers.size() == 0);
+    public static String[] solve(char[][] l, String p) throws FileNotFoundException {
+        Scanner s = new Scanner(new File(p));
+        String[] d = new String[Integer.parseInt(s.nextLine())];
+        for (int i = 0; i < d.length; i++)
+            d[i] = s.nextLine().toLowerCase();
+        s.close();
+        ArrayList<String> a = new ArrayList<>();
+        ArrayList<String> f = new ArrayList<>();
+        boolean[][] u = new boolean[l.length][];
+        for (int i = 0; i < u.length; i++)
+            u[i] = new boolean[l[i].length];
+        for (int i = 0; i < l.length; i++)
+            for (int j = 0; j < l[i].length; j++)
+                w(l[i][j] + "", f, l, d);
+        for (String w : f)
+            if (v(w, l, u))
+                a.add(w);
+        if (a.size() == 0) {
+            a = f;
+            int b = 2;
+            do solveLength(b++, a, l, d, u);
+            while (a.size() == 0);
         }
-        return answers.toArray(new String[answers.size()]);
+        return a.toArray(new String[a.size()]);
     }
-    public void solveLength(int numWords) {
-        int amnt = answers.size();
+    public static void solveLength(int n, ArrayList<String> a, char[][] l, String[] d, boolean[][] u) {
+        int amnt = a.size();
         for (int i = 0; i < amnt; i++) {
-            String answer = answers.get(0);
-            ArrayList<String> words = new ArrayList<>();
-            getWords(answer.charAt(answer.length() - 1) + "", words);
-            for (String word : words)
-                if (numWords != 2 || validSolution(answer + word))
-                    answers.addLast(answer + " - " + word);
-            answers.remove(0);
+            String r = a.get(0);
+            ArrayList<String> s = new ArrayList<>();
+            w(r.charAt(r.length() - 1) + "", s, l, d);
+            for (String w : s)
+                if (n != 2 || v(r + w, l, u))
+                    a.addLast(r + " - " + w);
+            a.remove(0);
         }
-        if (numWords > 2)
-            solveLength(numWords - 1);
+        if (n > 2)
+            solveLength(n - 1, a, l, d, u);
     }
-    private void getWords(String prefix, ArrayList<String> results) {
-        int currSide = 0;
-        for (int i = 0; i < letters.length; i++)
-            for (int j = 0; j < letters[i].length; j++)
-                if (prefix.charAt(prefix.length() - 1) == letters[i][j])
-                    currSide = i;
-        int range[] = {-1, -1};
-        for (int left = 0, right = dictionary.length, mid = left + (right - left) / 2;
-            left <= right; mid = left + (right - left) / 2) {
-            String midWord = dictionary[mid];
-            if (midWord.startsWith(prefix)) range[0] = (right = mid - 1) + 1;
-            else if (midWord.compareTo(prefix) < 0) left = mid + 1;
-            else right = mid - 1;
+    private static void w(String p, ArrayList<String> r, char[][] l, String[] d) {
+        int c = 0;
+        for (int i = 0; i < l.length; i++)
+            for (int j = 0; j < l[i].length; j++)
+                if (p.charAt(p.length() - 1) == l[i][j])
+                    c = i;
+        int g[] = {-1, -1};
+        for (int f = 0, t = d.length, m = f + (t - f) / 2;
+            f <= t; m = f + (t - f) / 2) {
+            String o = d[m];
+            if (o.startsWith(p)) g[0] = (t = m - 1) + 1;
+            else if (o.compareTo(p) < 0) f = m + 1;
+            else t = m - 1;
         }
-        if (range[0] != -1)
-            for(range[1] = range[0]; range[1] + 1 >= 0 && range[1] + 1 < dictionary.length
-                && dictionary[range[1] + 1].startsWith(prefix); range[1]++);
-        if (range[0] == -1)
+        if (g[0] != -1)
+            for(g[1] = g[0]; g[1] + 1 >= 0 && g[1] + 1 < d.length
+                && d[g[1] + 1].startsWith(p); g[1]++);
+        if (g[0] == -1)
             return;
-        if (this.dictionary[range[0]].equals(prefix))
-            results.add(prefix);
-        for (int i = 0; i < letters.length; i++)
-            if (i != currSide)
-                for (int j = 0; j < letters[i].length; j++)
-                    getWords(prefix + letters[i][j], results);
+        if (d[g[0]].equals(p))
+            r.add(p);
+        for (int i = 0; i < l.length; i++)
+            if (i != c)
+                for (int j = 0; j < l[i].length; j++)
+                    w(p+ l[i][j], r, l, d);
     }
-    private boolean validSolution(String solution) {
-        for (int i = 0; i < used.length; i++)
-            for (int j = 0; j < used[i].length; j++)
-                used[i][j] = false;
-        for (char c : solution.toCharArray())
-            for (int i = 0; i < letters.length; i++)
-                for (int j = 0; j < letters[i].length; j++)
-                    if (c == letters[i][j])
-                        used[i][j] = true;
-        for (boolean ba[] : used)
+    private static boolean v(String s, char[][] l, boolean[][] u) {
+        for (int i = 0; i < u.length; i++)
+            for (int j = 0; j < u[i].length; j++)
+                u[i][j] = false;
+        for (char c : s.toCharArray())
+            for (int i = 0; i < l.length; i++)
+                for (int j = 0; j < l[i].length; j++)
+                    if (c == l[i][j])
+                        u[i][j] = true;
+        for (boolean ba[] : u)
             for (boolean b : ba)
                 if (!b)
                     return false;
